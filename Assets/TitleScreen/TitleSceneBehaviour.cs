3452f52efdfef0c2c225a8c9f905297a8e77313c
五十嵐翔 <igara1119@gmail.com>
2016-08-03 00:34:13 +0900

    TitleのBehaviour

---
+++ Assets/TitleScreen/TitleSceneBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleSceneBehaviour : MonoBehaviour {
+
+	/**
+	 * インスタンス生成された時のみ実行されるメソッド
+	 */
+	void Awake () {
+	}
+
+	/**
+	 * Awakeの後で
+	 * 最初のフレームのアップデート前に実行されるメソッド
+	 */
+	void Start () {
+	}
+
+	/**
+	 * フレーム毎に一度実行されるメソッド
+	 */
+	void Update () {
+	}
+
+	/**
+	 * GUIイベントに応じて、フレームごとに複数回呼び出されるメソッド
+	 */
+	void OnGUI () {
+	}
+
+	/**
+	 * Behaviour が有効/アクティブになったときに呼び出される 
+	 */
+	void OnEnable() {
+	}
+
+	/**
+	 * Behaviour が無効/非アクティブになったときに呼び出される 
+	 */
+	void OnDisable() {
+	}
+}
